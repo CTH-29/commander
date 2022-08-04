2b161bb58ee1b858a63fc24273302e06a9ac07de
unknown <jiukui.yang@bitmain-inc.com>
2022-08-04 19:28:50 +0800

    update: working in process

--- commander/mainwindow.cpp
+++ commander/mainwindow.cpp
@@ -1,11 +1,23 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#define CMD_BTN_NUM 36 /* Button number */
+#define CMD_BTN_LINE_NUM 4 /* Button number each line */
+#define CMD_BTN_W 100 /* Button width */
+#define CMD_BTN_H 35  /* Button hight */
+#define CMD_BTN_X 10 /* Button start position x */
+#define CMD_BTN_Y 50  /* Button start position y */
+#define CMD_BTN_GAP 10  /* Button gap */
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    this->ctrl_state = 0;
+
+    settings=new QSettings("commander.ini", QSettings::IniFormat);
+    this->creat_commandui();
 }
 
 MainWindow::~MainWindow()
@@ -13,3 +25,36 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::creat_commandui()
+{
+    this->creat_button();
+    this->creat_tab();
+}
+
+void MainWindow::creat_button()
+{
+   int num=0;
+
+   for (num = 0; num < CMD_BTN_NUM; num++) {
+       cmdbtn = new QPushButton(this);
+       cmdbtn->setParent(this->ui->tabWidget);
+       cmdbtn->show();
+       cmdbtn->setGeometry(CMD_BTN_X + (CMD_BTN_GAP + CMD_BTN_W) * (num % CMD_BTN_LINE_NUM),
+                           CMD_BTN_Y + (CMD_BTN_GAP + CMD_BTN_H) * (num / CMD_BTN_LINE_NUM),
+                           CMD_BTN_W, CMD_BTN_H);
+       cmdbtn->setObjectName("cmdbtn"+QString::number((num)));
+   }
+}
+
+void MainWindow::creat_tab()
+{
+    int i;
+    int tap_num;
+
+    tap_num = settings->value("BaseInfo/tapnum").toInt();
+
+    for (i = 1; i < tap_num; i++) {
+        this->ui->tabWidget->addTab(new QWidget(), "tap" + QString::number(i + 1));
+    }
+
+}
--- commander/mainwindow.h
+++ commander/mainwindow.h
@@ -2,6 +2,12 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QKeyEvent>
+#include <QSettings>
+#include <QClipboard>
+#include <QDebug>
+#include <QMessageBox>
+#include <QPushButton>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -17,5 +23,15 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    int ctrl_state;
+    QSettings *settings;
+    QPushButton *cmdbtn;
+
+    void creat_commandui();
+    void creat_button();
+    void creat_tab();
+protected:
+//    virtual void keyPressEvent(QKeyEvent *ev);
+//    virtual void keyReleaseEvent(QKeyEvent *ev);
 };
 #endif // MAINWINDOW_H
